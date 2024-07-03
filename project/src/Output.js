feba00c0b47cba04f9011801ad6f3f1109547f82
Ayush Upadhyay <ayushhupadhyay@gmail.com>
2024-07-03 12:51:48 +0530

    Delete project directory
    In order to upload new updated files and folders into the repository created locally for implementing the project

--- project/src/Output.js
+++
@@ -1,8 +0,0 @@
-function Output(){
-    return(
-        <div>
-            
-        </div>
-    )
-}
-export default Output;
--- project/src/footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return(
-        <footer className="footer">
-            Under Development by Ayush Upadhyay
-        </footer>
-    )
-}
-export default Footer;
