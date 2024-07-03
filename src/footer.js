965d3aaad424f22c21e0c28f2483bcf9cf1a6390
Ayush Upadhyay <ayushhupadhyay@gmail.com>
2024-07-03 13:18:59 +0530

    Added files App.css, etc from local storage
    Added files App.css
    App.test.js
    footer.js
    index.css
    index.js
    isl_dictionary.json
    navbar.js
    reportWebVitals.js
    setupTests.js
    SignLanguageDisplay.js

---
+++ src/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer className="footer">
+            Ayush Upadhyay @ 2024
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+import './App.css';
+function Navbar() {
+    return(
+        <nav className="n"><h1> SPEECHSIGN-AI: SPEECH TO INDIAN SIGN LANGUAGE CONVERTER</h1> 
+        </nav>
+    )
+}
+
+export default Navbar;
